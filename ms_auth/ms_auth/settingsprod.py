610e39e5d1ddec98505688392cb846b8865c4cfd
Javid <jav.alakbarli@gmail.com>
2025-02-15 15:28:22 +0400

    updated settingsprod file

--- ms_auth/ms_auth/settingsprod.py
+++ ms_auth/ms_auth/settingsprod.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['142.93.163.78']
+ALLOWED_HOSTS = ['auth.smarteval.com']
 
 
 # Application definition
